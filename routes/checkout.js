2f35c69a31089aa66c912a88622176bf4cb93f04
Mingfeng Li <lmf950122@gmail.com>
2021-06-27 07:33:38 +0000

    Create checkout routes

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const checkoutQueries = require('../db/checkoutQueries');
-const { checkout } = require('./users');
 
 router.get('/', (req, res) => {
   checkoutQueries.getCheckout()
