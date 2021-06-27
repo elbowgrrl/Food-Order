e7adfa671ede8b3732b391afd6797b17bc7a0f46
Mingfeng Li <lmf950122@gmail.com>
2021-06-27 07:33:52 +0000

    Create index routes

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const productQueries = require('../db/productQueries');
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-
 router.get('/', (req, res) => {
   productQueries.getProducts()
     .then((products) => {
