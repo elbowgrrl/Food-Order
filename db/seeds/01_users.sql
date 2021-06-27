6efd6a631bc18e177ec202b1b0d6c57e63a76dca
Mingfeng Li <lmf950122@gmail.com>
2021-06-27 07:20:28 +0000

    Created users fake data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, phone, email, password)
+VALUES ('Alice', '123-456-7890','a@a.com', 'password'),
+('mingfeng', '123-123-7890','m@a.com', 'password' ),
+('Byenajae', '911-911-9111','b@a.com', 'passwword');
