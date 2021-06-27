cfe5b88d4b5bc671512f25deb23ad2778adfee41
Mingfeng Li <lmf950122@gmail.com>
2021-06-27 07:31:43 +0000

    Create product queries for get requet

--- db/productQueries.js
+++ db/productQueries.js
@@ -1,8 +1,9 @@
+const { response } = require('express');
 const db = require('./db');
 
 const getProducts = () => {
   return db
-    .query('SELECT * FROM widgets;')
+    .query('SELECT * FROM items;')
     .then((response) => {
       console.log("reponse", response.rows);
       return response.rows;
@@ -12,6 +13,18 @@ const getProducts = () => {
     });
 };
 
+const getProductsById = (id) => {
+  return db
+    .query('SELECT * FROM items WHERE id = 1')
+    .then((response) => {
+      return response.rows[0];
+    })
+    .catch((error) => {
+      console.error("Error message:", error);
+    });
+}
+
 module.exports = {
-  getProducts
+  getProducts,
+  getProductsById
 };
