6282052e8fe30277b54dfd6225beba037be55f03
Mingfeng Li <lmf950122@gmail.com>
2021-06-27 07:27:12 +0000

    Create payments fake data

---
+++ db/seeds/07_payments.sql
@@ -0,0 +1,4 @@
+INSERT INTO payments (order_id, creditcard, expiryDate, cvv_number, total_amount);
+VALUES(1, '123', '2012-08-19', 123, 10000);
+(3, '366', '2011-01-15', 432, 20030),
+(2, '788', '2021--7-20', 312, 1300);
