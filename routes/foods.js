4e6b0cd45d41647d6a833d9a68c04a823a63f427
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-06-28 11:43:12 -0700

    add routes and query

---
+++ routes/foods.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    const query = `SELECT * FROM foods`;
+    db.query(query)
+      .then(data => {
+        const foods = data.rows;
+        res.json({ foods });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
