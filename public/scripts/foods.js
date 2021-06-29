8446a52506e41deed0af4bb1cd0f720006e3c982
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-06-28 18:57:45 -0700

    commit before pull

--- public/scripts/foods.js
+++ public/scripts/foods.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 $(() => {
-  console.log("ready!");
+  console.log("foods");
 
   // fetch the products
   const loadFoods = () => {
@@ -8,7 +8,7 @@ $(() => {
     $.get(`/api/foods`)
       .then((foods) => {
         console.log(foods);
-        renderFoods(foods.reverse());
+        renderFoods(foods);
       });
   };
 
@@ -19,6 +19,7 @@ $(() => {
       <div class="foods">
         <h2>Product Name: ${food.name}</h2>
         <h3>Price: $${food.price}</h3>
+        <img src="${food.url_image}" alt="">
       </div>
     `);
     return $food;
@@ -35,5 +36,20 @@ $(() => {
 
   loadFoods();
 });
+
+const object = {};
+
+// object['name'] = food.name;
+// object['price'] = food.price;
+// object['foodId'] = food.id;
+// object['quantity'] = 1;
+// object['totalPrice'] = food.price * quantity;
+
+
+
+
+
+
+
 
 
