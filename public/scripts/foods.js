fab0ea90ebf24c9adc411596f229b6ae0a8e5a94
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-06-30 19:06:51 -0700

    change class to make css work

--- public/scripts/foods.js
+++ public/scripts/foods.js
@@ -51,8 +51,8 @@ $(() => {
         <img class="food-image" src="${food.url_image}">
       </body>
       <footer class="menu-footer">
-        <button class='add-to-cart' id="${food.id}">Add to cart</button>
-        <button class="more-info-button" id='${food.id}'>More info</button>
+        <button class='confirm add-to-cart' id="${food.id}">Add to cart</button>
+        <button class="more-info" id='${food.id}'>More info</button>
       </footer>
     </article>
     `;
