1017ecf4d5ed23b79204bf4308d02d8c382970d2
Mingfeng Li <lmf950122@gmail.com>
2021-07-01 04:19:55 +0000

    Retrieve more order information from database with query

--- routes/admin.js
+++ routes/admin.js
@@ -16,12 +16,19 @@ const adminRoutes = (db) => {
   router.get("/:orderid", (req, res) => {
     const reqParams = req.params.orderid;
     db.query(`
-    SELECT orders.id, foods.name, SUM(foods.price) AS total_price
+    SELECT orders.id,
+           foods.name,
+           SUM(foods.price*quantity) AS total_price,
+           order_foods.quantity AS quantity,
+           foods.price AS price
     FROM orders
     JOIN order_foods ON order_foods.order_id = orders.id
     JOIN foods ON food_id = foods.id
     WHERE orders.id = $1
-    GROUP BY orders.id, foods.name
+    GROUP BY orders.id,
+             foods.name,
+             order_foods.quantity,
+             foods.price
     ;`, [reqParams])
       .then((response) => {
         res.json(response.rows);
